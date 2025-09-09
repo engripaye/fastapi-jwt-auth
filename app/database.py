e11e281cc13366b9dab0535ea5404d2053676ab9
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-09-09 22:22:45 +0100

    backend code commit by ENGR. IPAYE

--- app/database.py
+++ app/database.py
@@ -8,14 +8,15 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 
 # Needed for SQLite to allow multiple threads (dev only)
-connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else{}
+connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
 
 engine = create_engine(DATABASE_URL, echo=False, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-#Utility dependency to get DB session
+
+# Utility dependency to get DB session
 def get_db():
     db = SessionLocal()
     try:
