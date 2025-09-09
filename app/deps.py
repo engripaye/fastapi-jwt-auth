05b70209ca670c99d7bb5198f1ee654eb13b398a
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-09-09 22:53:30 +0100

    backend code commit by ENGR. IPAYE

---
+++ app/deps.py
@@ -0,0 +1,46 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from . import models, auth
+from .database import get_db
+from .schemas import TokenData
+
+
+oauth2_scheme =  OAuth2PasswordBearer(tokenUrl="token")
+
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(models.User).filter(models.User.username == username).first()
+
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+
+def authenticate_user(db: Session, username: str, password: str, verify_password_func):
+    user = get_user_by_username(db, username)
+    if not user:
+        return None
+    if not verify_password_func(password, user.hashed_password)
+        return None
+    return user
+
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    payload = auth.decode_access_token(token)
+    if payload is None
+        raise credentials_exception
+    username: str = payload.get("sub")
+    if username is None:
+        raise credentials_exception
+    user = db.query(models.User).filter(models.User.username == username).first()
+    if user is None:
+        raise credentials_exception
+    return user
+
+
