17702ae2c24558067fad3e3239d12f73cdc709b5
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-09-10 05:52:35 +0100

    backend code commit by ENGR. IPAYE

--- app/deps.py
+++ app/deps.py
@@ -2,8 +2,8 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from app import models
-import auth
-from database import get_db
+from app import auth
+from app.database import get_db
 from .schemas import TokenData
 
 
