2aace09edb6cea22b9e285f7911ce3e2de798326
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-09-10 08:29:32 +0100

    backend code commit by ENGR. IPAYE

--- app/deps.py
+++ app/deps.py
@@ -1,7 +1,7 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
-from app import models
+from app import models, security
 from app import auth
 from app.database import get_db
 from .schemas import TokenData
@@ -28,20 +28,16 @@ def authenticate_user(db: Session, username: str, password: str, verify_password
 
 
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    payload = auth.decode_access_token(token)
-    if payload is None:
-        raise credentials_exception
-    username: str = payload.get("sub")
+    username = security.verify_access_token(token)
     if username is None:
-        raise credentials_exception
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
     user = db.query(models.User).filter(models.User.username == username).first()
-    if user is None:
-        raise credentials_exception
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
     return user
 
 
--- app/security.py
+++ app/security.py
@@ -4,11 +4,11 @@ from typing import Optional
 import os
 from dotenv import load_dotenv
 
-# Load environment variables
 load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY", "changeme")  # fallback if .env missing
+SECRET_KEY = os.getenv("SECRET_KEY", "changeme")
 ALGORITHM = "HS256"
+
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
@@ -16,3 +16,14 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+def verify_access_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            return None
+        return username
+    except Exception:
+        return None
